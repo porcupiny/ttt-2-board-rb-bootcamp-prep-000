46ecea6b2ab4028cbb8de077b355b658ad8c586d
Liane Chan <liane.chan@gmail.com>
2018-05-27 19:49:01 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -28,8 +28,4 @@ row[7]
 # Position 9: Right 
 row[8]
 
-# Move X to the Position 2, Middle
-row[1] = "X"
-row #=> [" ", "X", " "]
-
 board = [row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8]]
